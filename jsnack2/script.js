fc0e2815db0fa21fb37f28f457c776eb27f2f4b5
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2024-12-03 12:24:34 +0100

    feat: start ex2

---
+++ jsnack2/script.js
@@ -0,0 +1,16 @@
+//In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+//1. Array con i numeri degli invitati
+const guests = [
+    'Nathan',
+    'Ed',
+    'Fabio',
+    'Phil',
+    'Carlo',
+    'Lewis',
+    'Luca'
+  ];
+
+//2. Chiede all'utente il suo nome
+
+//3. Variabile per verificare se il nome 
