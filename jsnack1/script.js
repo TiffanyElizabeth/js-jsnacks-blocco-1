8f312534c086c242583db58514172ef0306fda7e
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2024-12-03 11:36:51 +0100

    feat: start ex1

---
+++ jsnack1/script.js
@@ -0,0 +1,11 @@
+// L’utente inserisce due numeri in successione, con due prompt.
+// Il software stampa il maggiore
+
+//1. chiede all'utente un numero
+const num1 = Number(prompt("Scrivi un numero"))
+
+//2. chiede all'utente un altro numero
+const num2 = Number(prompt("Scrivi un altro numero"))
+
+//3. stampa il maggiore numero 
+if 
