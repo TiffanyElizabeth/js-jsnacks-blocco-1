8a9957717ab236e522ab27cb3e3116a98506d016
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2024-12-03 11:39:57 +0100

    feat: ex1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -2,10 +2,16 @@
 // Il software stampa il maggiore
 
 //1. chiede all'utente un numero
-const num1 = Number(prompt("Scrivi un numero"))
+const num1 = Number(prompt("Scrivi un numero"));
 
 //2. chiede all'utente un altro numero
-const num2 = Number(prompt("Scrivi un altro numero"))
+const num2 = Number(prompt("Scrivi un altro numero"));
 
 //3. stampa il maggiore numero 
-if 
+if (num1 > num2) {
+    alert("Il maggiore numero è: " + num1)
+} else if (num2 > num1) {
+    alert("Il maggiore numero è: " + num2)
+} else {
+    alert("I numeri inseriti sono uguali.")
+}
